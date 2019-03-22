c2be39aefe6b70acf065a0147477e6569ce02f51
Thomas Lacher <thomas.lacher@fxinnovation.com>
2019-03-22 12:32:28 -0400

    Refactor code ' Extract GetClient Add parameter to rocketchat host and credentials in a separate file'

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
-	"github.com/RocketChat/Rocket.Chat.Go.SDK/realtime"
 	"github.com/prometheus/alertmanager/template"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
+)
+
+var (
+	configFile = kingpin.Flag("config.file", "RocketChat configuration file.").Default("config/rocketchat.yml").String()
 )
 
 // Webhook http response
@@ -30,11 +32,10 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 	// Do not forget to close the body at the end
 	defer r.Body.Close()
 
-	// Do stuff here
-	log.Printf("Alerts: Status=%s, GroupLabels=%v, CommonLabels=%v", data.Status, data.GroupLabels, data.CommonLabels)
-	for _, alert := range data.Alerts {
-		log.Printf("Alert: status=%s,Labels=%v,Annotations=%v", alert.Status, alert.Labels, alert.Annotations)
-	}
+	rocketChatClient := GetRocketChatClient(*configFile)
+
+	// Format notifications and send it
+	SendNotification(rocketChatClient, data)
 
 	// Returns a 200 if everything went smoothly
 	sendJSONResponse(w, http.StatusOK, "Success")
@@ -44,6 +45,9 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 // - first one to give a status on the receiver itself
 // - second one to actually process the data
 func main() {
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
 	http.HandleFunc("/webhook", webhook)
 	http.Handle("/metrics", promhttp.Handler())
 
@@ -51,8 +55,6 @@ func main() {
 	if os.Getenv("PORT") != "" {
 		listenAddress = ":" + os.Getenv("PORT")
 	}
-
-	//getClient("prometheus", "prometheus@local.local", "to_be_modified")
 
 	log.Printf("listening on: %v", listenAddress)
 	log.Fatal(http.ListenAndServe(listenAddress, nil))
@@ -69,34 +71,3 @@ func sendJSONResponse(w http.ResponseWriter, status int, message string) {
 	w.Write(bytes)
 }
 
-
-func getClient(name, email, password string) {
-
-	host := "gossip.dazzlingwrench.fxinnovation.com"
-	rtClient, errClient := realtime.NewClient(&url.URL{Scheme: "https", Host: host}, false)
-	if errClient != nil {
-		log.Printf("Error to get realtime client: %v", errClient)
-		return
-	}
-
-	credentials := &models.UserCredentials{Name: name, Email: email, Password: password}
-
-	_, errUser := rtClient.Login(credentials)
-	if errUser != nil {
-		log.Printf("Error to login user: %v", errUser)
-		return
-	}
-
-	roomID, errRoom := rtClient.GetChannelId("prometheus-test-room")
-	if errRoom != nil {
-		log.Printf("Error to get room ID: %v", errRoom)
-		return
-	}
-	channel := &models.Channel{ID: roomID}
-	_, errMessage := rtClient.SendMessage(channel, "Test AlertManager")
-	if errMessage != nil {
-		log.Printf("Error to send message: %v", errMessage)
-		return
-	}
-
-}
---
+++ rocketchat.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
+	"github.com/RocketChat/Rocket.Chat.Go.SDK/realtime"
+	"github.com/prometheus/alertmanager/template"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"net/url"
+)
+
+
+type Config struct {
+	Rocketchat  url.URL
+	Credentials models.UserCredentials
+}
+
+
+func GetRocketChatClient (configFile string) *realtime.Client {
+
+	config := Config{}
+
+	// Load the config from the file
+	configData, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	errYAML := yaml.Unmarshal([]byte(configData), &config)
+	if errYAML != nil {
+		log.Fatalf("Error: %v", errYAML)
+	}
+
+	rtClient, errClient := realtime.NewClient(&config.Rocketchat, false)
+	if errClient != nil {
+		log.Printf("Error to get realtime client: %v", errClient)
+		return nil
+	}
+
+	_, errUser := rtClient.Login(&config.Credentials)
+	if errUser != nil {
+		log.Printf("Error to login user: %v", errUser)
+		return nil
+	}
+
+	return rtClient
+
+}
+
+
+// Function connects to RocketChat server, authenticate the user and send the notification
+func SendNotification(rtClient *realtime.Client, data template.Data) {
+
+	channelID, errRoom := rtClient.GetChannelId(data.CommonLabels["channel_name"])
+	if errRoom != nil {
+		log.Printf("Error to get room ID: %v", errRoom)
+		return
+	}
+	channel := &models.Channel{ID: channelID}
+
+	log.Printf("Alerts: Status=%s, GroupLabels=%v, CommonLabels=%v", data.Status, data.GroupLabels, data.CommonLabels)
+	for _, alert := range data.Alerts {
+		message := fmt.Sprintf("Alert: status=%s,Labels=%v,Annotations=%v", alert.Status, alert.Labels, alert.Annotations)
+		log.Printf(message)
+
+		_, errMessage := rtClient.SendMessage(channel, message)
+		if errMessage != nil {
+			log.Printf("Error to send message: %v", errMessage)
+			return
+		}
+	}
+}
