71545af7b7328b9bab9c5f653e53ae5dda42f7ce
ZakiChammaa <zakichammaa@gmail.com>
2019-03-30 12:49:53 -0400

    added function to format message

--- rocketchat.go
+++ rocketchat.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/url"
+	"strings"
+	"time"
+
 	"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
 	"github.com/RocketChat/Rocket.Chat.Go.SDK/realtime"
 	"github.com/prometheus/alertmanager/template"
 	"gopkg.in/yaml.v2"
-	"io/ioutil"
-	"log"
-	"net/url"
 )
 
-
+// Config struct for Rocketchat credentials and url
 type Config struct {
 	Rocketchat  url.URL
 	Credentials models.UserCredentials
 }
 
-
-func GetRocketChatClient (configFile string) *realtime.Client {
+// GetRocketChatClient takes path to config file and returns *realtime.Client
+func GetRocketChatClient(configFile string) *realtime.Client {
 
 	config := Config{}
 
@@ -49,8 +53,54 @@ func GetRocketChatClient (configFile string) *realtime.Client {
 
 }
 
+// Need to remove this function once our PR to Rocket.Chat SDK is approved
+func newRandomID() string {
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf("%f", rand.Float64())
+}
 
-// Function connects to RocketChat server, authenticate the user and send the notification
+func formatMessage(channel *models.Channel, alert template.Alert) *models.Message {
+
+	const (
+		warning       = "warning"
+		critical      = "critical"
+		warningColor  = "#f2e826"
+		criticalColor = "#ef0b1e"
+		defaultColor  = "#ffffff"
+	)
+
+	var color string
+	title := fmt.Sprintf("**%s: %s**", strings.Title(alert.Status), alert.Annotations["summary"])
+	attachementText := fmt.Sprintf("**description**: %s\n", alert.Annotations["description"])
+
+	if strings.ToLower(alert.Status) == warning {
+		color = warningColor
+	} else if strings.ToLower(alert.Status) == critical {
+		color = criticalColor
+	} else {
+		color = defaultColor
+	}
+
+	for k, v := range alert.Labels {
+		attachementText += fmt.Sprintf("**%s**: %s\n", k, v)
+	}
+
+	return &models.Message{
+		ID:     newRandomID(),
+		RoomID: channel.ID,
+		Msg:    title,
+		PostMessage: models.PostMessage{
+			Attachments: []models.Attachment{
+				models.Attachment{
+					Color: color,
+					Text:  attachementText,
+				},
+			},
+		},
+	}
+}
+
+// SendNotification connects to RocketChat server, authenticate the user and send the notification
 func SendNotification(rtClient *realtime.Client, data template.Data) {
 
 	channelID, errRoom := rtClient.GetChannelId(data.CommonLabels["channel_name"])
@@ -62,10 +112,10 @@ func SendNotification(rtClient *realtime.Client, data template.Data) {
 
 	log.Printf("Alerts: Status=%s, GroupLabels=%v, CommonLabels=%v", data.Status, data.GroupLabels, data.CommonLabels)
 	for _, alert := range data.Alerts {
-		message := fmt.Sprintf("Alert: status=%s,Labels=%v,Annotations=%v", alert.Status, alert.Labels, alert.Annotations)
-		log.Printf(message)
 
-		_, errMessage := rtClient.SendMessage(channel, message)
+		message := formatMessage(channel, alert)
+
+		_, errMessage := rtClient.SendMessage(message)
 		if errMessage != nil {
 			log.Printf("Error to send message: %v", errMessage)
 			return
